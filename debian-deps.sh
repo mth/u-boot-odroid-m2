03b1ad2aee421be480fd563d301a2f6e4a15ed1a
mth <mth@localhost>
2025-01-18 12:58:48 +0200

    Debian dependencies

---
+++ debian-deps.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+apt-get install gcc gcc-aarch64-linux-gnu build-essential bison libssl-dev libgnutls28-dev device-tree-compiler python3-libfdt python3-pkg-resources python3-pyelftools fdisk
