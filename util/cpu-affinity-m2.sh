75129dce0667974ba4a969142f3083ba7920f537
mth <mth@localhost>
2025-05-01 14:44:35 +0300

    Adjust a bit irq affinity script

--- util/cpu-affinity-m2.sh
+++ util/cpu-affinity-m2.sh
@@ -15,13 +15,17 @@ irq_affinity() {
 # Many interrupts are handled by by default only on CPU0.
 # Spreading devices with heavier usage to other cores gives better performance.
 # Here the A55 cores are used to leave the big cores idle on background activity.
-irq_affinity vop 1
+irq_affinity dma-controller 1
 irq_affinity xhci-hcd:usb 1
+irq_affinity vop 2
 irq_affinity end0 2
 irq_affinity fd880000.i2c 3
 
 # OpenGL performance seems to be better with panthor-job on an A76 core.
 irq_affinity panthor-job 5
+
+# Bulk of USB-3/USB-C processing seems to happen with dwc3 interrupts, use another A76 core for that
+irq_affinity dwc3 6
 
 # Apply the IRQ CPU affinity rules
 awk -F: "$AFFINITY_AWK" < /proc/interrupts | sh
