b964378f46e13269bdf47498349bee4707944608
BBMaciek <50260373+BBMaciek@users.noreply.github.com>
2026-03-07 15:37:03 +0100

    Update debian-deps.sh
    Adding missing dependency package - flex

--- debian-deps.sh
+++ debian-deps.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-apt-get install gcc gcc-aarch64-linux-gnu build-essential bison libc6-dev libssl-dev libgnutls28-dev device-tree-compiler python3-libfdt python3-pkg-resources python3-pyelftools fdisk
+apt-get install gcc gcc-aarch64-linux-gnu build-essential bison libc6-dev libssl-dev libgnutls28-dev device-tree-compiler python3-libfdt python3-pkg-resources python3-pyelftools fdisk flex
